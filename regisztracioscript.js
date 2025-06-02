84a01f4a88ffdfa6493c6d7fe8e4376cefa3a7c2
Bo1129 <bohmb.22d@acsjszki.hu>
2025-06-02 09:32:36 +0200

    Regisztrációs oldal - az újakezdés

--- regisztracioscript.js
+++ regisztracioscript.js
@@ -1,20 +1,12 @@
-function UserValidation(){
-    var fnev = document.getElementById("felhaszanlonev").value;
-    var email = document.getElementById("email").value;
-    var jelszo = document.getElementById("jelszo").value;
-    var mjelszo = document.getElementById("mjelszo").value;
+const form = document.querySelector('#form');
 
-    if(fnev === ''){
-        document.getElementById("message").innerHTML = "Felhasználónév megadása kötelező";
-    }
-    if(email === ''){
-        document.getElementById("message").innerHTML = "Email-cím megadása kötelező";
-    }
-    if(jelszo === ''){
-        document.getElementById("message").innerHTML = "Jelszo megadása kötelező";
-    }
-    if(jelszo != mjelszo){
-        document.getElementById("message").innerHTML = "A két jelszó nem egyezik";
-    }
-    
-}
+const user = document.querySelector('#felhasznalonev');
+
+const email = document.querySelector('#email');
+
+const password = document.querySelector('#jelszo');
+
+const check = document.querySelector('#mjelszo');
+
+let registeredUser = {};
+
